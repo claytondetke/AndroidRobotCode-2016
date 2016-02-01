a2a0b338c80b16b18ca22b29c21bd7a7f656d874
claytondetke <cjdet_000@10.35.48.35>
2016-02-01 16:38:24 -0500

    Completed Collector Subsystem and added a button to dashboard for toggle

--- AndroidRobotCode-2016/src/org/usfirst/frc/team868/robot/OI.java
+++ AndroidRobotCode-2016/src/org/usfirst/frc/team868/robot/OI.java
@@ -2,7 +2,7 @@ package org.usfirst.frc.team868.robot;
 
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
-import org.usfirst.frc.team868.robot.commands.TestMotorsCommand;
+import org.usfirst.frc.team868.robot.commands.*;
 
 /**
  * This class is the glue that binds the controls on the physical operator
@@ -52,6 +52,7 @@ public class OI {
 	
 	public void initSmartDashboard(){
 		SmartDashboard.putData("Drive_Forward", new TestMotorsCommand(.3));
+		SmartDashboard.putData("Toggle_Collector", new CollectorCommand());
 	}
 }
 
