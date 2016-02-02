2fda1fb67854653fa5d06df3a6b404c1345e8dbc
Caleb <Caleb>
2016-02-01 16:41:35 -0800

    Add New Abstraction Model
    Add new abstract class which inherits MotorSubsystem, named
    DriveMotorSubsystem, which is inherited by the DriveMotorSubsystems.

--- AndroidRobotCode-2016/src/org/usfirst/frc/team868/robot/subsystems/LeftDriveMotorSubsystem.java
+++ AndroidRobotCode-2016/src/org/usfirst/frc/team868/robot/subsystems/LeftDriveMotorSubsystem.java
@@ -5,19 +5,19 @@ import org.usfirst.frc.team868.robot.RobotMap;
 /**
  *
  */
-public class LeftMotorSubsystem extends MotorSubsystem {
+public class LeftDriveMotorSubsystem extends DriveMotorSubsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-	public LeftMotorSubsystem() { //constructor to set default values
-		isInverted  = true;
+	public LeftDriveMotorSubsystem() { //constructor to set default values
+		isInverted = true;
 		MOTOR_PORT = RobotMap.LEFT_DRIVE_MOTOR;
 	}
 	
     public static MotorSubsystem getInstance() {
     	if(instance == null) {
-    		instance = new LeftMotorSubsystem();
+    		instance = new LeftDriveMotorSubsystem();
     	}
     	
     	return instance;
--- AndroidRobotCode-2016/src/org/usfirst/frc/team868/robot/subsystems/RightDriveMotorSubsystem.java
+++ AndroidRobotCode-2016/src/org/usfirst/frc/team868/robot/subsystems/RightDriveMotorSubsystem.java
@@ -5,19 +5,19 @@ import org.usfirst.frc.team868.robot.RobotMap;
 /**
  *
  */
-public class RightMotorSubsystem extends MotorSubsystem {
+public class RightDriveMotorSubsystem extends DriveMotorSubsystem {
     
     // Put methods for controlling this subsystem
     // here. Call these from Commands.
 
-	public RightMotorSubsystem() { //constructor to set default values
-		isInverted  = false;
+	public RightDriveMotorSubsystem() { //constructor to set default values
+		isInverted = false;
 		MOTOR_PORT = RobotMap.RIGHT_DRIVE_MOTOR;
 	}
 	
     public static MotorSubsystem getInstance() {
     	if(instance == null) {
-    		instance = new RightMotorSubsystem();
+    		instance = new RightDriveMotorSubsystem();
     	}
     	
     	return instance;
