43f4457bec63c9013fa0888e06cc3fe522200c48
claytondetke <cjdet_000@10.35.48.35>
2016-02-02 19:15:51 -0500

    Added PopperCommand with basic toggle capability

---
+++ AndroidRobotCode-2016/src/org/usfirst/frc/team868/robot/commands/PopperCommand.java
@@ -0,0 +1,45 @@
+package org.usfirst.frc.team868.robot.commands;
+
+import org.usfirst.frc.team868.robot.subsystems.PopperSubsystem;
+
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ */
+public class PopperCommand extends Command {
+	
+	private PopperSubsystem popper;
+	private boolean toggle;
+
+    public PopperCommand() {
+    	popper = PopperSubsystem.getInstance();
+    	requires(popper);
+    	toggle = !popper.getPosition();
+        // Use requires() here to declare subsystem dependencies
+        // eg. requires(chassis);
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+    	popper.setPopper(toggle);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return false;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
